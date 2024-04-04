83cb13081885bb7a64431b6cdae934b139b3f5a6
lbayer10 <lenny.bayer7@gmail.com>
2024-04-04 20:44:57 +0800

    create home and tokenomics page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,95 @@
+const HomePage = () => {
+    return (
+        <div>
+            <div className="w-full h-5/6">
+                <img className="w-full h-[400px] lg:h-[800px] object-cover object-center" src={require("../../assets/images/banner.jpg")} alt="" />
+                <div className="absolute top-0 flex flex-col items-center justify-center w-full h-[400px] lg:h-[800px]">
+                    <h1 className="text-4xl lg:text-6xl font-semibold">Launching Soon</h1>
+                    <button className="px-8 py-4 rounded-md bg-[#FFF] text-black font-semibold uppercase mt-10">Apply for an IDO</button>
+                </div>
+            </div>
+            <div className="mx-4 lg:mx-40">
+                <div>
+                    <div className="text-center text-4xl lg:text-5xl font-semibold my-10">Trust Launchpad Partners</div>
+                    <hr className="m-auto border-[#355396] border-[1.5px] w-16" />
+                    <div className="mt-5 grid sm:grid-cols-1 lg:grid-cols-3 gap-20">
+                        <div>
+                            <h6 className="text-center text-xl my-5">Pinksale</h6>
+                            <img className="w-full h-full" src={require("../../assets/images/pinksale-2000x1000.webp")} alt="" />
+                        </div>
+                        <div>
+                            <h6 className="text-center text-xl my-5">DEXView</h6>
+                            <img className="w-full h-full" src={require("../../assets/images/DEXView.jpg")} alt="" />
+                        </div>
+                        <div>
+                            <h6 className="text-center text-xl my-5">ASSURE DEFI</h6>
+                            <img className="w-full h-full" src={require("../../assets/images/Assure defi logo.png")} alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div className="pt-32 lg:pt-48">
+                    <div className="text-center text-4xl lg:text-6xl font-semibold my-10">Trust Launchpad</div>
+                    <hr className="m-auto border-[#355396] border-[1.5px] w-16" />
+                    <div className="mt-10 lg:mt-0 grid grid-cols-1 lg:grid-rows-3 gap-10 lg:gap-20">
+                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-20">
+                            <div className="flex items-center">
+                                <img className="w-full lg:h-96" src={require("../../assets/images/TLP10.png")} alt="" />
+                            </div>
+                            <div className="flex flex-col justify-center">
+                                <h1 className="text-center text-4xl lg:text-6xl font-semibold my-4 lg:my-8">About Us</h1>
+                                <p className="text-md lg:text-2xl font-normal text-center">Trust launchpad is decentralised multi chain crowd funding platform built for web3 projects, empowering web3 projects to distribute tokens and raise liquidity. Allowing projects to explore new ideas and expand their horizons</p>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-20">
+                            <div className="flex flex-col justify-center">
+                                <h1 className="text-center text-4xl lg:text-6xl font-semibold my-4 lg:my-8">Our Mission</h1>
+                                <p className="text-md lg:text-2xl font-normal text-center">Trust Launchpad aims to eliminate scams by bringing only trusted projects to investors</p>
+                            </div>
+                            <div className="flex items-center">
+                                <img className="w-full lg:h-96" src={require("../../assets/images/TLP1.png")} alt="" />
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-20">
+                            <div>
+                                <img className="w-full lg:h-96" src={require("../../assets/images/TLP2.png")} alt="" />
+                            </div>
+                            <div className="flex flex-col justify-center">
+                                <h1 className="text-center text-4xl lg:text-6xl font-semibold my-4 lg:my-8">Our Aim</h1>
+                                <div className="text-md lg:text-2xl font-normal text-center">
+                                    <p className="mb-10">Trust Launchpad eliminates tier barriers, by allowing TRUST token stakers to have a guaranteed allocation in the presale.</p>
+                                    <p className="mb-10">Get early access to the ideas of tomorrow.</p>
+                                    <p className="mb-10">Highly-vetted Web3 projects you can trust. Supported by industry-leading creators and funds.</p>
+                                    <p className="mb-10">Our aim is to be the pre-eminent place to launch your crypto project.</p>
+                                </div>
+                            </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="pt-10 lg:pt-20">
+                    <div className="text-center text-4xl lg:text-6xl font-semibold my-10">Follow Us On Social Media</div>
+                    <hr className="m-auto border-[#355396] border-[1.5px] w-16" />
+                    <div className="lg:flex justify-center mt-10">
+                        <div className="flex flex-col justify-center items-center my-8 mx-10">
+                            <img className="rounded-full mb-5 w-60 lg:w-96" src={require("../../assets/images/TLP8.png")} alt="" />
+                            <div className="m-auto flex flex-col items-center w-full lg:w-[400px]">
+                                <p className="text-center lg:text-4xl font-semibold">Telegram</p>
+                                <p className="text-center lg:text-xl my-10">Join our Telegram. <br /> Meet the community, participate in the discussion and fun activities.</p>
+                                <button className="px-8 py-4 rounded-md bg-[#FFF] text-black font-semibold uppercase">Join Telegram</button>
+                            </div>
+                        </div>
+                        <div className="flex flex-col justify-center items-center my-8 mx-10">
+                            <img className="rounded-full mb-5 w-60 lg:w-96" src={require("../../assets/images/TLP5.png")} alt="" />
+                            <div className="m-auto flex flex-col items-center w-full lg:w-[220px]">
+                                <p className="text-center lg:text-4xl font-semibold">X(Twitter)</p>
+                                <p className="text-center lg:text-xl my-10">Keep up to date with our PR and Marketing Join the conversation.</p>
+                                <button className="px-8 py-4 rounded-md bg-[#FFF] text-black font-semibold uppercase">Follow Us</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export { HomePage };
---
+++ src/pages/tokenomics/index.tsx
@@ -0,0 +1,96 @@
+const TokenomicsPage = () => {
+    return (
+        <div>
+            <div className="mx-4 lg:mx-40 mt-40">
+                <div>
+                    <div className="text-center text-4xl lg:text-6xl font-semibold my-10">$Trust Tokenomics</div>
+                    <hr className="m-auto border-[#355396] border-[1.5px] w-16" />
+                    <div className="mt-5 grid sm:grid-cols-1 lg:grid-cols-2 gap-20">
+                        <div>
+                            <h6 className="text-center text-2xl lg:text-4xl font-semibold my-5">Tokenomics</h6>
+                            <img className="w-full h-3/6" src={require("../../assets/images/DEXView.jpg")} alt="" />
+                            <div className="mt-5">
+                                <p className="text-md lg:text-xl font-medium text-center">Our token will launch on the BSC chain. Only 1 Billion tokens will be issued.</p>
+                                <div className="text-md lg:text-xl text-center font-medium mt-10">
+                                    <p>Taxs</p>
+                                    <p>Buy tax - 2%</p>
+                                    <p>Sell tax - 5%</p>
+                                    <p>All tax to marketing wallet</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <h6 className="text-center text-2xl lg:text-4xl font-semibold my-5">Token Allocation</h6>
+                            <img className="w-full h-3/6" src={require("../../assets/images/TLP Tokenomics.png")} alt="" />
+                            <div className="mt-5 text-md lg:text-xl text-center font-medium">
+                                <p>PRESALE - 400,000,000</p>
+                                <p>DEX LIQUIDITY - 200,000,000</p>
+                                <p>STAKING - 150,000,000</p>
+                                <p>CEX - 90,000,000</p>
+                                <p>ECOSYSTEM - 50,000,000</p>
+                                <p>TEAM - 80,000,000</p>
+                                <p>ADVISOR - 30,000,000</p>
+                                <p className="mt-10">Our Contract in Published and Verified</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-20 mt-28">
+                        <div className="flex items-center">
+                            <img className="w-full lg:h-96" src={require("../../assets/images/TLP6.png")} alt="" />
+                        </div>
+                        <div className="flex flex-col justify-center">
+                            <h1 className="text-center text-4xl lg:text-5xl font-semibold my-4 lg:my-8">Staking & Revenue Sharing</h1>
+                            <p className="text-md lg:text-xl font-normal text-center">Earn rewards and participate in private sales and IDOs.</p>
+                            <p className="text-md lg:text-xl font-normal text-center mt-10">Put your $TRUST token to work by locking it in the staking dapp and earn a share 35% of Trust Launchpad's net income.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-20 lg:mt-32">
+                    <h1 className="text-center text-4xl lg:text-6xl font-semibold my-4 lg:my-8">Trust Launchpad Roadmap</h1>
+                    <hr className="m-auto border-[#355396] border-[1.5px] w-16" />
+                    <div className="lg:flex justify-between mt-10">
+                        <div className="text-lg lg:text-xl text-center">
+                            <h1 className="text-2xl my-10 lg:text-3xl mb-5">Phase 1</h1>
+                            <p>Logo and Brand Design</p>
+                            <p>BSC contract deployment</p>
+                            <p>Adult and KYC</p>
+                            <p>Whitepaper</p>
+                        </div>
+                        <div className="text-lg lg:text-xl text-center">
+                            <h1 className="text-2xl my-10 lg:text-3xl mb-5">Phase 2</h1>
+                            <p>Community development</p>
+                            <p>Partnership with influencers</p>
+                            <p>Twitter Campaign</p>
+                            <p>Pre launch marketing</p>
+                            <p>Presale</p>
+                        </div>
+                        <div className="text-lg lg:text-xl text-center">
+                            <h1 className="text-2xl my-10 lg:text-3xl mb-5">Phase 3</h1>
+                            <p>Staking release</p>
+                            <p>P2P development</p>
+                            <p>Listing on PancakeSwap</p>
+                            <p>Multi chain launchpad release</p>
+                        </div>
+                    </div>
+                    <div className="lg:flex justify-around mt-20">
+                        <div className="text-lg lg:text-xl text-center">
+                            <h1 className="text-2xl my-10 lg:text-3xl mb-5">Phase 4</h1>
+                            <p>Marketing and partners</p>
+                            <p>Coingecko listing</p>
+                            <p>First tier1 CEK listing</p>
+                        </div>
+                        <div className="text-lg lg:text-xl text-center">
+                            <h1 className="text-2xl my-10 lg:text-3xl mb-5">Phase 5</h1>
+                            <p>CMC listing</p>
+                            <p>Chinese and Spanish KOLs</p>
+                            <p>Ambassador program</p>
+                            <p>P2P release</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export { TokenomicsPage };
